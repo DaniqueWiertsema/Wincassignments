4a5b2270e51484dcee60867d9c6e057dfc058e6c
DaniqueWiertsema <79660925+DaniqueWiertsema@users.noreply.github.com>
2021-03-19 09:59:56 +0100

    Add files via upload
    Strings main.py (tweede poging)

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-scorer0 = 'Ruud Gullit'
-scorer1 = 'Marco van Basten'
-goal0 = 32
-goal1 = 54
-scorers = 'Ruud Gullit' + '32',  'Marco van Basten' + '54'
-report = f'{scorer0} scored in the {goal0}nd minute' + f'\n{scorer1} scored in the {goal1}th minute'
+scorer_0 = 'Ruud Gullit'
+scorer_1 = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = scorer_0 + " " + str(goal_0) + "," + " " + scorer_1 + " " + str(goal_1)
+report = f'{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute'
+
 player = 'Ruud Gullit'
-first_name =  player[0:4]
-last_name_len = len(player[5:11])
-name_short = player[0] + '. ' + player [5:11]
-chant = (first_name + '!')
-good_chant = (first_name + '!') != chant
+first_name = player[:player.find(" ")]
+last_name_len = len(player[player.find(" "):-1])
+name_short = player[0] + '.' + player[player.find(" "):]
+chant = (first_name + "!" + " ") * (len(first_name)-1) + first_name + "!"
+good_chant = chant[-1] != " "
 print(good_chant)
+
+
+
+
+
+
+
